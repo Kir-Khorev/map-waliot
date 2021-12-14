8ebef07a3c1e7971e0b889dd359233457b2230d9
Кирилл Хорев <khorev.spb@gmail.com>
2021-12-14 16:57:18 +0300

    first commit

---
+++ src/Map.js
@@ -0,0 +1,20 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import './App.css';
+
+export default function Map (props) {
+    return (
+        <div id="map">
+            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[51.505, -0.09]}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </div>
+    )
+}
