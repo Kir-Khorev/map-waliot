62e3c42b3db765ae32be13cbc93a4a82f442cf7a
Кирилл Хорев <khorev.spb@gmail.com>
2021-12-21 16:38:08 +0300

    added DisplayPosition for Cars Object

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import './App.css';
 import Map from './components/Map/Map';
-import CarsList from './components/Cars/CarsList';
 import Footer from './components/Footer';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <Map />
-      {/* <CarsList /> */}
       <Footer />
     </div>
   );
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,11 +1,46 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef, useCallback, useMemo } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import './Map.css';
 import CarsItem from '../Cars/CarsItem/CarsItem';
 
-export default function Map(props) {
 
+export default function Map(props) {
     const [cars, setCars] = useState([]);
+
+    function DisplayPosition({ map }) {
+        const zoom = 10
+
+        const [position, setPosition] = useState(map.getCenter())
+
+        const onClickFunc = useCallback((myCenter) => {
+            map.setView(myCenter, zoom)
+        }, [map])
+
+        const onMove = useCallback(() => {
+            setPosition(map.getCenter())
+        }, [map])
+
+        useEffect(() => {
+            map.on('move', onMove)
+            return () => {
+                map.off('move', onMove)
+            }
+        }, [map, onMove])
+
+        return (
+            <div div className='carsList'>
+                <h1>Объекты</h1>
+                {
+                    cars.map(e => {
+                        const centerThis = [e.latitude, e.longitude]
+                        return <div onClick={() => onClickFunc(centerThis)} >
+                            <CarsItem carsList={cars} key={e.id} id={e.id} name={e.name} latitude={e.latitude} longitude={e.longitude} />
+                        </div>
+                    })
+                }
+            </div>
+        )
+    }
 
     // Load data from API
     useEffect(() => {
@@ -19,42 +54,35 @@ export default function Map(props) {
             })
     }, []);
 
-    const localFunc = (e) => {
-        console.log('click #', e);
-        cars.map(i => {
-            if (i.id === e) {
-                console.log('chose one', i.id)
-            }
-        })
-        return
-    };
+    const [map, setMap] = useState(null)
+
+    const displayMap = useMemo(
+        () => (
+            <MapContainer center={[51.505, -0.09]} zoom={4}
+                scrollWheelZoom={true}
+                whenCreated={setMap}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {
+                    cars.map(elem => {
+                        return <Marker key={elem.id} position={[elem.latitude, elem.longitude]} >
+                            <Popup>{elem.name}, {elem.id}</Popup>
+                        </Marker>
+                    })
+                }
+            </MapContainer>
+        ),
+        [],
+    )
 
     return (
         <div className='mapWrapper'>
             <div id="map">
-                <MapContainer center={[51.505, -0.09]} zoom={4} scrollWheelZoom={false}>
-                    <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                    {
-                        cars.map(elem => {
-                            return <Marker key={elem.id} position={[elem.latitude, elem.longitude]} >
-                                <Popup>{elem.name}, {elem.id}</Popup>
-                            </Marker>
-                        })
-                    }
-                </MapContainer>
+                {displayMap}
             </div>
-            <div div className='carsList'>
-                <h1>Объекты</h1>
-                {
-                    cars.map(e => {
-                        return <div onClick={() => localFunc(e.id)}>
-                            <CarsItem onClick={localFunc} carsList={cars} key={e.id} id={e.id} name={e.name} latitude={e.latitude} longitude={e.longitude} />
-                        </div>
-                    })
-                }
-            </div>
+            {map ? <DisplayPosition map={map} /> : null}
         </div>
     )
 }
